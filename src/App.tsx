20bc1509fd5bb1b6c6aa697846bb4f65d4dd068c
hnochx <hnochx@gmail.com>
2024-10-09 16:45:29 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">App Component</h1>
+      <h1 className="text-3xl font-bold">App Component1</h1>
     </>
   );
 }
